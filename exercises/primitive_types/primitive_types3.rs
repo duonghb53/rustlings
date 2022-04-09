aa7ef94bdead5f540cee8d1ceb516f9093c0f426
duonghb53 <duonghb53@gmail.com>
2022-04-10 01:28:07 +0700

    completed primitive type

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,8 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a = [121,3,3,3,41,1,51,51,515,15,15,151,13,13];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
