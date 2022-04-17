7d72cc6b3816054d48afe5833e0f4ab7fa572346
duonghb53 <duonghb53@gmail.com>
2022-04-18 01:26:50 +0700

    enum

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,12 +1,21 @@
 // enums2.rs
 // Make me compile! Execute `rustlings hint enums2` for hints!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
+    Quit,
+    Move { x: u8, y: u8 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
 }
+
+pub struct Coordinate {
+    x: u8,
+    y: u8,
+}
+
+struct Color(u8, u8, u8);
 
 impl Message {
     fn call(&self) {
